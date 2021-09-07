2de29f1d9b66495f21a67418f2428c150e136908
pesdesigner <pauloeduardo.dg@outlook.com>
2021-09-07 10:38:50 -0300

    Project created

---
+++ backend/src/test/java/com/graficoeweb/gwvendas/GwvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graficoeweb.gwvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
