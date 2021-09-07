d1a16eef6ad9992b41700f2131a081552b5ad5fc
pesdesigner <pauloeduardo.dg@outlook.com>
2021-09-07 15:41:48 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+            <p className="text-light">App desenvolvido por <a href="https://github.com/pesdesigner" target="_blank" rel="noreferrer">Paulo Eduardo</a></p>
+            <p className="text-light"><small><strong>Spring Boot | React App</strong><br/>
+            Todos os direitos reservados: <a href="https://graficoeweb.com.br" target="_blank" rel="noreferrer">graficoeweb.com.br</a></small></p>
+        </div>
+        </footer>
+    );
+}
+
+export default Footer;
