d4d9e68b24fe4968df256cd1cf822d28c0b3b37e
pesdesigner <pauloeduardo.dg@outlook.com>
2021-09-12 19:16:18 -0300

    Group by search

--- backend/src/main/java/com/graficoeweb/gwvendas/service/SaleService.java
+++ backend/src/main/java/com/graficoeweb/gwvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.graficoeweb.gwvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.graficoeweb.gwvendas.dto.SaleDTO;
+import com.graficoeweb.gwvendas.dto.SaleSuccessDTO;
+import com.graficoeweb.gwvendas.dto.SaleSumDTO;
 import com.graficoeweb.gwvendas.entities.Sale;
 import com.graficoeweb.gwvendas.repositories.SaleRepository;
 import com.graficoeweb.gwvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
