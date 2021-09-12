2c7eeb1514fdce02f46db35d5063cf7b879b30d9
pesdesigner <pauloeduardo.dg@outlook.com>
2021-09-12 14:08:30 -0300

    Layers

---
+++ backend/src/main/java/com/graficoeweb/gwvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.graficoeweb.gwvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.graficoeweb.gwvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+	
+}
